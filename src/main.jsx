6c0c2422ed3004777ed6295b4960b65a4ec96799
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-08-21 18:02:13 -0400

    Use the Router instead of App

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import Router from "./Router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Router></Router>
   </React.StrictMode>
 );
