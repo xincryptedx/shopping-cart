6d02ccd47b78c1313b78a0c9053369f51efcd24d
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-08-21 19:44:41 -0400

    Implement custom hook

--- src/routes/products.jsx
+++ src/routes/products.jsx
@@ -1,7 +1,29 @@
+import useFetchPokemon from "../hooks/useFetchPokemon";
+
 const Products = () => {
+  const { pokemon, error, loading } = useFetchPokemon();
+
+  console.log(pokemon);
+
+  if (error)
+    return (
+      <div>
+        <p>Loading error!</p>
+      </div>
+    );
+
+  if (loading)
+    return (
+      <div>
+        <p>Loading info...</p>
+      </div>
+    );
+
   return (
     <div>
       <p>This is the products route!</p>
+      <p>Here is some data on {pokemon[0].name}</p>
+      <p>{JSON.stringify(pokemon[0])}</p>
     </div>
   );
 };
