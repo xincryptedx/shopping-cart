d26b2382e9edee541db01b05509cfff9a68c296c
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-08-29 16:10:23 -0400

    Add and implement css module for main layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,14 +1,15 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar/NavBar";
+import styles from "./MainLayout.module.css";
 
 const MainLayout = () => {
   return (
-    <>
+    <div className={styles.MainLayout}>
       <NavBar />
       <main>
         <Outlet />
       </main>
-    </>
+    </div>
   );
 };
 
