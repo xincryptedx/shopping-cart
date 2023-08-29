fead6fc65814604f06bba5b4889897f56376359a
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-08-29 15:41:08 -0400

    Add test text

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 const MainLayout = () => {
   return (
     <>
-      {/* Nav */}
+      <p>Im a layout!</p>
       <main>
         <Outlet />
       </main>
