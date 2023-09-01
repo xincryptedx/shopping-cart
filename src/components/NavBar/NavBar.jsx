a4b3d9b78d1ba253eb3fbef5eb105e0c28b79c32
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-01 16:18:50 -0400

    Move nav link to drop down menu div

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,11 +5,16 @@ import logoSmall from "../../assets/logoIconSmall.svg";
 const NavBar = () => {
   return (
     <nav className={styles.NavBar}>
-      <Link to="products" className={styles.productsBtn}>
-        Shop
-      </Link>
+      <button className={styles.menuBtn}>Menu</button>
+      <div className={styles.navLinks}>
+        <ul>
+          <Link to="products" className={styles.productsBtn}>
+            Prize Counter
+          </Link>
+        </ul>
+      </div>
       <Link to="/" className={styles.homeBtn}>
-        <img src={logoSmall} alt="" />
+        <img src={logoSmall} alt="Rocket Game Corner home button" />
       </Link>
       <button className={styles.cartBtn}>Cart</button>
     </nav>
