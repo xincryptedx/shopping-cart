a62ad366b48e4a8d17b355dcb914eb5f60673955
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-01 15:58:32 -0400

    Remove unneeded classes

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,13 +5,13 @@ import logoSmall from "../../assets/logoIconSmall.svg";
 const NavBar = () => {
   return (
     <nav className={styles.NavBar}>
-      <Link to="products" className={styles.productsBtn + " " + styles.navBtn}>
+      <Link to="products" className={styles.productsBtn}>
         Shop
       </Link>
       <Link to="/" className={styles.homeBtn}>
         <img src={logoSmall} alt="" />
       </Link>
-      <button className={styles.cartBtn + " " + styles.navBtn}>Cart</button>
+      <button className={styles.cartBtn}>Cart</button>
     </nav>
   );
 };
