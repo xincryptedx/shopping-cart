ae22efe0daa82553960a27738b81b99cb243220d
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-01 17:26:35 -0400

    Add custom hook that takes generic params to handle click outside of ref element

---
+++ src/hooks/useClickOutside.js
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+
+const useClickOutside = (ref, state, callback) => {
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        callback();
+      }
+    };
+
+    if (state === true) {
+      document.addEventListener("click", handleClickOutside);
+    } else {
+      document.removeEventListener("click", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [ref, state, callback]);
+};
+
+export default useClickOutside;
