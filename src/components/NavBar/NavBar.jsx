77df9ef4e2808d34e1d4d10f0947514a62363aae
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-01 15:46:05 -0400

    Swap products and home button locations

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,11 +4,11 @@ import styles from "./NavBar.module.css";
 const NavBar = () => {
   return (
     <nav className={styles.NavBar}>
-      <Link to="/" className={styles.homeBtn + " " + styles.navBtn}>
-        Home
-      </Link>
       <Link to="products" className={styles.productsBtn + " " + styles.navBtn}>
         Shop
+      </Link>
+      <Link to="/" className={styles.homeBtn + " " + styles.navBtn}>
+        Home
       </Link>
       <button className={styles.cartBtn + " " + styles.navBtn}>Cart</button>
     </nav>
