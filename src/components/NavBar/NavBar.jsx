3f875c1b1b1e5fa77164804ea4e209574a9865f8
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-02 15:22:28 -0400

    Remove nav links and related code for showing/hiding

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,49 +1,19 @@
 import { Link } from "react-router-dom";
 import styles from "./NavBar.module.css";
 import logoSmall from "../../assets/logoIconSmall.svg";
-import { useRef } from "react";
 import PropTypes from "prop-types";
-import useClickOutside from "../../hooks/useClickOutside";
 
-const NavBar = ({ navLinksOpen, setNavLinksOpen }) => {
-  const linksRef = useRef(null);
-
+const NavBar = ({ setNavLinksOpen }) => {
   const toggleNavLinksOpen = (event) => {
     event.stopPropagation();
     setNavLinksOpen((previous) => !previous);
   };
-
-  const closeNavLinks = () => {
-    setNavLinksOpen(false);
-  };
-
-  // Closes nav links by setting state when click outside of div
-  useClickOutside(linksRef, navLinksOpen, closeNavLinks);
 
   return (
     <nav className={styles.NavBar}>
       <button className={styles.menuBtn} onClick={(e) => toggleNavLinksOpen(e)}>
         Menu
       </button>
-      <div
-        className={
-          navLinksOpen
-            ? styles.navLinksOpen + " " + styles.navLinks
-            : styles.navLinks
-        }
-        ref={linksRef}
-      >
-        <ul>
-          <Link
-            to="products"
-            className={styles.productsBtn}
-            onFocus={() => setNavLinksOpen(true)}
-            onBlur={() => setNavLinksOpen(false)}
-          >
-            Prize Counter
-          </Link>
-        </ul>
-      </div>
       <Link to="/" className={styles.homeBtn}>
         <img src={logoSmall} alt="Rocket Game Corner home button" />
       </Link>
