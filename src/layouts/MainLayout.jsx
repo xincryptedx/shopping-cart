ba1195660dfd7908452394a01d9029cbfd63f50e
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-05 15:32:50 -0400

    Add ShoppingCart component and state for managing its classes

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
-import NavBar from "../components/NavBar/NavBar";
-import NavLinks from "../components/NavLinks/NavLinks";
 import styles from "./MainLayout.module.css";
 import { useState } from "react";
+import NavBar from "../components/NavBar/NavBar";
+import NavLinks from "../components/NavLinks/NavLinks";
+import ShoppingCart from "../components/ShoppingCart/ShoppingCart";
 
 const MainLayout = () => {
   const [navLinksOpen, setNavLinksOpen] = useState(false);
+  const [shoppingCartOpen, setShoppingCartOpen] = useState(false);
 
   return (
     <div className={styles.MainLayout}>
@@ -18,6 +20,14 @@ const MainLayout = () => {
         }
         navLinksOpen={navLinksOpen}
         setNavLinksOpen={setNavLinksOpen}
+      />
+      <ShoppingCart
+        className={
+          shoppingCartOpen
+            ? styles.ShoppingCartOpen + " " + styles.ShoppingCart
+            : styles.ShoppingCart
+        }
+        setNavLinksOpen={setShoppingCartOpen}
       />
       <main>
         <Outlet />
