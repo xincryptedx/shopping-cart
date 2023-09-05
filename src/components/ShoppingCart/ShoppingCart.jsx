5e77d5ebdc9d744af0ef5ee964a02b7472e349ff
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-05 19:22:18 -0400

    Use custom hook to close on escape

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,8 +1,12 @@
 import styles from "./ShoppingCart.module.css";
 import cancelIcon from "../../assets/cancel.svg";
 import PropTypes from "prop-types";
+import useSetStateOnKeydown from "../../hooks/useSetStateOnKeydown";
 
-const ShoppingCart = ({ className, setShoppingCartOpen }) => {
+const ShoppingCart = ({ className, shoppingCartOpen, setShoppingCartOpen }) => {
+  // Close ShoppingCart on escape
+  useSetStateOnKeydown("Escape", shoppingCartOpen, setShoppingCartOpen, false);
+
   return (
     <aside
       className={
@@ -22,6 +26,7 @@ const ShoppingCart = ({ className, setShoppingCartOpen }) => {
 
 ShoppingCart.propTypes = {
   className: PropTypes.string,
+  shoppingCartOpen: PropTypes.bool.isRequired,
   setShoppingCartOpen: PropTypes.func.isRequired,
 };
 
