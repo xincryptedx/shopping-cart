f1e5e703fc00c34b938a3de3b68e5401bed826f9
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-06 17:08:13 -0400

    Add classes to cta p elements

--- src/routes/root/root.jsx
+++ src/routes/root/root.jsx
@@ -4,12 +4,12 @@ const Root = () => {
   return (
     <div className={styles.rootRoute}>
       <section className={styles.callToAction}>
-        <p>
+        <p className={styles.cta1}>
           Rocket Game Corner is a convenient option for busy trainers who wish
           to expand their Pokemon collection.
         </p>
-        <p>Shop Now!</p>
-        <p>
+        <p className={styles.cta2}>Shop Now!</p>
+        <p className={styles.cta3}>
           Our unique approach allows us to provide Pokemon at competitive rates
           from sources aligned with our values.
         </p>
