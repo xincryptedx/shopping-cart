4900529184f9f4446e8df617a440b2016b9b209c
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-09 17:08:35 -0400

    Remove unused classNames

--- src/routes/root/root.jsx
+++ src/routes/root/root.jsx
@@ -5,12 +5,12 @@ const Root = () => {
     <div className={styles.rootRoute}>
       <section className={styles.callToAction}>
         <div className={styles.ctaBackgroundImage} aria-hidden />
-        <p className={styles.cta1}>
+        <p>
           Rocket Game Corner is a convenient option for busy trainers who wish
           to expand their Pokemon collection.
         </p>
         <p className={styles.cta2}>Shop Now!</p>
-        <p className={styles.cta3}>
+        <p>
           Our unique approach allows us to provide Pokemon at competitive rates
           from sources aligned with our values.
         </p>
