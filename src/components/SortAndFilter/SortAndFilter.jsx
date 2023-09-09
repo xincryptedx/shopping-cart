40c5fcaddfc3203524e22e9fc47ea7e7e8a207fb
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-09 19:59:16 -0400

    Add basic component outline with placeholder buttons

---
+++ src/components/SortAndFilter/SortAndFilter.jsx
@@ -0,0 +1,12 @@
+import styles from "./SortAndFilter.module.css";
+
+const SortAndFilter = () => {
+  return (
+    <section className={styles.SortAndFilter}>
+      <button>Sort</button>
+      <button>Filter</button>
+    </section>
+  );
+};
+
+export default SortAndFilter;
