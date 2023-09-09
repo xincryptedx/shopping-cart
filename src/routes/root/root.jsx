5fc3046a86629916a2ae3f2746e4961d427c2a27
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-09 17:22:36 -0400

    Use new CallToAction component instead of raw html

--- src/routes/root/root.jsx
+++ src/routes/root/root.jsx
@@ -1,20 +1,10 @@
+import CallToAction from "../../components/CallToAction/CallToAction";
 import styles from "./root.module.css";
 
 const Root = () => {
   return (
     <div className={styles.rootRoute}>
-      <section className={styles.callToAction}>
-        <div className={styles.ctaBackgroundImage} aria-hidden />
-        <p>
-          Rocket Game Corner is a convenient option for busy trainers who wish
-          to expand their Pokemon collection.
-        </p>
-        <p className={styles.cta2}>Shop Now!</p>
-        <p>
-          Our unique approach allows us to provide Pokemon at competitive rates
-          from sources aligned with our values.
-        </p>
-      </section>
+      <CallToAction />
     </div>
   );
 };
