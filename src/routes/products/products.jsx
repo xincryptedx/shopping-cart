b9d7bc257497e2b4c322a2d9c066a779bee7d050
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-09 18:39:34 -0400

    Remove conditional logic based on props

--- src/routes/products/products.jsx
+++ src/routes/products/products.jsx
@@ -1,28 +1,10 @@
 import PropTypes from "prop-types";
 import styles from "./products.module.css";
 
-const Products = ({ pokemon, error, loading }) => {
-  if (error)
-    return (
-      <div>
-        <p>Loading error!</p>
-      </div>
-    );
-
-  if (loading)
-    return (
-      <div>
-        <p>Loading info...</p>
-      </div>
-    );
-
+const Products = () => {
   return (
     <div className={styles.productsRoute}>
       <p>This is the products route!</p>
-      <p>Pokemon have been loaded!</p>
-      {pokemon.map((mon) => (
-        <p key={mon.id}>{mon.name}</p>
-      ))}
     </div>
   );
 };
