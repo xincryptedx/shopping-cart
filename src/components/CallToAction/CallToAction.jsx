20fed44f176bd9a334db805ec1e076566024225f
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-09 17:13:04 -0400

    Copy html from root

---
+++ src/components/CallToAction/CallToAction.jsx
@@ -0,0 +1,20 @@
+import styles from "./CallToAction.module.css";
+
+const CallToAction = () => {
+  return (
+    <section className={styles.callToAction}>
+      <div className={styles.ctaBackgroundImage} aria-hidden />
+      <p>
+        Rocket Game Corner is a convenient option for busy trainers who wish to
+        expand their Pokemon collection.
+      </p>
+      <p className={styles.cta2}>Shop Now!</p>
+      <p>
+        Our unique approach allows us to provide Pokemon at competitive rates
+        from sources aligned with our values.
+      </p>
+    </section>
+  );
+};
+
+export default CallToAction;
