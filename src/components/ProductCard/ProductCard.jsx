9ec2c0ddbe82c1c5b91975d3af94adef24e31bf3
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-09 20:39:54 -0400

    Change section to div

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -2,11 +2,11 @@ import styles from "./ProductCard.module.css";
 
 const ProductCard = () => {
   return (
-    <section className={styles.ProductCard}>
+    <div className={styles.ProductCard}>
       <p>Pokemon Picture</p>
       <p>Pokemon Name</p>
       <p>Price</p>
-    </section>
+    </div>
   );
 };
 
