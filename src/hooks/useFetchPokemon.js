07de66ce7cc3576dd7eca2cdb49036d7464d57a4
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-10 18:37:12 -0400

    Alter hook to retrieve generation of pokemon from 1-9

--- src/hooks/useFetchPokemon.js
+++ src/hooks/useFetchPokemon.js
@@ -1,12 +1,17 @@
 import { useEffect, useState } from "react";
 
-const useFetchPokemon = () => {
+const useFetchPokemon = (generation = 1) => {
+  //There are only nine generations as of now
+  generation = Math.min(Math.max(1, generation), 9);
+
   const [pokemon, setPokemon] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon/151", { mode: "cors" })
+    fetch(`https://pokeapi.co/api/v2/generation/${generation}`, {
+      mode: "cors",
+    })
       .then((response) => {
         if (response.status >= 400) {
           throw new Error("server error");
