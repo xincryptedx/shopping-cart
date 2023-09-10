32079d5c7ea2ce941068ea41957a824920058e96
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-10 19:41:58 -0400

    Add state for tracking errors and loading status

--- src/hooks/usePreparePokemon.js
+++ src/hooks/usePreparePokemon.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 const usePreparePokemon = (pokemon) => {
   const [shopPokemon, setShopPokemon] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   // Select some random pokemon
   // Query the data, format, and push to shopPokemon for each selected pokemon
