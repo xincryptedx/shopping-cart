093dad65acee35725a280a5bcc1d3dc79776f6fa
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-09 20:01:05 -0400

    Add SortAndFilter component

--- src/routes/products/products.jsx
+++ src/routes/products/products.jsx
@@ -1,8 +1,10 @@
+import SortAndFilter from "../../components/SortAndFilter/SortAndFilter";
 import styles from "./products.module.css";
 
 const Products = () => {
   return (
     <div className={styles.productsRoute}>
+      <SortAndFilter />
       <p>This is the products route!</p>
     </div>
   );
