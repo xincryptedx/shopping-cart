a0c81e0b3e37749a9f98c7b80eac0fc193f986f3
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-10 19:30:55 -0400

    Use custom hook to fetch pokemon generation data

--- src/routes/products/products.jsx
+++ src/routes/products/products.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
 import ProductDeck from "../../components/ProductDeck/ProductDeck";
 import SortAndFilter from "../../components/SortAndFilter/SortAndFilter";
+import useFetchPokemon from "../../hooks/useFetchPokemon";
 import styles from "./products.module.css";
 
 const Products = () => {
+  const [generation, setGeneration] = useState(1);
+  // Fetch pokemon data for given generation
+  const {
+    pokemon,
+    loading: pokemonLoading,
+    error: pokemonError,
+  } = useFetchPokemon(generation);
+
+  // Prepare pokemon data
+
   return (
     <div className={styles.productsRoute}>
       <SortAndFilter />
