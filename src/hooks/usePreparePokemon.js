33a866a343ab325603bfddbeef157f8625e8b3f5
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-10 19:32:42 -0400

    Add custom hook for preparing pokemon data for use by cards

---
+++ src/hooks/usePreparePokemon.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+const usePreparePokemon = (pokemon) => {
+  const [shopPokemon, setShopPokemon] = useState([]);
+
+  // Select some random pokemon
+  // Query the data, format, and push to shopPokemon for each selected pokemon
+  // After they are loaded set loading to false
+};
+
+export default usePreparePokemon;
