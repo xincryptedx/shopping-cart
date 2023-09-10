8ba1654938c1247be3c21f4e692adfd8bc12b4a0
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-09 20:28:26 -0400

    Fix className

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -2,7 +2,7 @@ import styles from "./ProductCard.module.css";
 
 const ProductCard = () => {
   return (
-    <section className={styles.ProductDeck}>
+    <section className={styles.ProductCard}>
       <p>Pokemon Picture</p>
       <p>Pokemon Name</p>
       <p>Price</p>
