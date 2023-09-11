56cef65dd1b22e15f65c60fc9aada9ac246763e7
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-11 15:36:45 -0400

    Add function for returning a new shuffled array from input array

---
+++ src/utilities/fisherShuffle.js
@@ -0,0 +1,12 @@
+const shuffleArray = (inputArray) => {
+  const shuffledArray = [...inputArray];
+
+  for (let i = shuffledArray.length - 1; i > 0; i -= 1) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+  }
+
+  return shuffledArray;
+};
+
+export default shuffleArray;
