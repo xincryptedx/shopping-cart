db431e144e9e984bb9d8b745cc0e2adabb404812
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-11 18:13:04 -0400

    Add logic for rendering when there is an error

--- src/routes/products/products.jsx
+++ src/routes/products/products.jsx
@@ -22,8 +22,24 @@ const Products = () => {
   } = usePreparePokemon(pokemon);
 
   // If loading show dummy deck
+  if (pokemonLoading || shopPokemonLoading) {
+    return <p>Loading...</p>;
+  }
 
   // If error show error message
+  if (pokemonError || shopPokemonError) {
+    return (
+      <>
+        <p>An error occured.</p>
+        {pokemonError ? (
+          <p>Pokemon Load Error: {pokemonError.toString()} </p>
+        ) : null}
+        {shopPokemonError ? (
+          <p>Pokemon Details Load Error: {shopPokemonError.toString()}</p>
+        ) : null}
+      </>
+    );
+  }
 
   // If all data loaded show deck with cards for each prepared pokemon
   return (
