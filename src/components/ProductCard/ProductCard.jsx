ccf1fb225837ab92568f7a10a2cf9defef8a3379
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-11 21:29:55 -0400

    Add proptypes for pokemon data prop

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,7 +1,8 @@
 import styles from "./ProductCard.module.css";
 import pokedollar from "../../assets/pokedollarIcon.svg";
+import PropTypes from "prop-types";
 
-const ProductCard = () => {
+const ProductCard = ({ pokemonData }) => {
   return (
     <div className={styles.ProductCard}>
       <img src="" alt="" className={styles.image} />
@@ -14,6 +15,10 @@ const ProductCard = () => {
       />
     </div>
   );
+};
+
+ProductCard.propTypes = {
+  pokemonData: PropTypes.object,
 };
 
 export default ProductCard;
