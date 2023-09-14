dd99d123104c50a37806b7fb3cb0e70401eebbe0
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-13 22:23:17 -0400

    Install and config vtest eslint plugin because eslint is an asshole

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,26 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true, vitest: true },
+  env: { browser: true, es2020: true },
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
+    "plugin:vitest/recommended",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "vitest"],
   rules: {
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
     ],
+  },
+  globals: {
+    vi: "readonly",
+    describe: "readonly",
+    it: "readonly",
   },
 };
