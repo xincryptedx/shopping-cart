677ebd00f210b396b2adc805c882f27a1aca7d45
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-13 21:48:05 -0400

    Install eslint

--- src/hooks/useFetchPokedex.js
+++ src/hooks/useFetchPokedex.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import shuffleArray from "../utilities/fisherShuffle";
 
 const useFetchPokedex = (region = "kanto") => {
   const [pokemon, setPokemon] = useState(null);
@@ -41,57 +40,10 @@ const useFetchPokedex = (region = "kanto") => {
       }
     };
 
-    const getPokemonDetails = async (pokedexData) => {
-      // Select random stock amount
-      const minStock = 10;
-      const maxStock = 30;
-      const stockNum =
-        Math.floor(Math.random() * (maxStock - minStock + 1)) + minStock;
-
-      // Create randomized array of pokemon
-      const shuffledPokedexData = shuffleArray(pokedexData.pokemon_entries);
-      const chosenPokemon = [];
-
-      // For inStock amount of times pop a pokemon and add to chosen array
-      for (let i = 0; i < stockNum; i += 1) {
-        chosenPokemon.push(shuffledPokedexData.pop());
-      }
-
-      // Async fetch details for all chosenPokemon
-      const promises = chosenPokemon.map(async (entry) => {
-        try {
-          const response = await fetch(
-            `https://pokeapi.co/api/v2/pokemon/${entry.pokemon_species.name}`,
-            {
-              mode: "cors",
-            }
-          );
-          if (response.status >= 400) {
-            throw new Error("pokemon fetch error");
-          }
-          return response.json();
-        } catch (error) {
-          setError(error);
-          return null;
-        }
-      });
-
-      const pokemonDetailsData = await Promise.all(promises);
-
-      const filteredPokemonData = pokemonDetailsData.filter(
-        (data) => data !== null
-      );
-
-      setPokemon(filteredPokemonData);
+    getPokedexData(validatedRegion).catch((error) => {
+      setError(error);
       setLoading(false);
-    };
-
-    getPokedexData(validatedRegion)
-      .then((response) => getPokemonDetails(response))
-      .catch((error) => {
-        setError(error);
-        setLoading(false);
-      });
+    });
   }, [validatedRegion, region]);
 
   return { pokemon, error, loading };
