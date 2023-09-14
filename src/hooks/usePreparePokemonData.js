aa2011e2d3cdf06a8a4cb9389e5d3a6b5a07b876
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-14 19:26:13 -0400

    Refashion logic for getting pokemon details into custom hook

--- src/hooks/usePreparePokemonData.js
+++ src/hooks/usePreparePokemonData.js
@@ -1,48 +1,66 @@
+import { useEffect, useState } from "react";
 import shuffleArray from "../utilities/fisherShuffle";
 
-const usePreparePokemonData = () => {
-  const getPokemonDetails = async (pokedexData) => {
-    // Select random stock amount
-    const minStock = 10;
-    const maxStock = 30;
-    const stockNum =
-      Math.floor(Math.random() * (maxStock - minStock + 1)) + minStock;
+const usePreparePokemonData = (pokedexData) => {
+  const [pokemonData, setPokemonData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    // Create randomized array of pokemon
-    const shuffledPokedexData = shuffleArray(pokedexData.pokemon_entries);
-    const chosenPokemon = [];
+  useEffect(() => {
+    const getPokemonDetails = async (pokedexData) => {
+      // Select random stock amount
+      const minStock = 10;
+      const maxStock = 30;
+      const stockNum =
+        Math.floor(Math.random() * (maxStock - minStock + 1)) + minStock;
 
-    // For inStock amount of times pop a pokemon and add to chosen array
-    for (let i = 0; i < stockNum; i += 1) {
-      chosenPokemon.push(shuffledPokedexData.pop());
-    }
+      // Create randomized array of pokemon
+      const shuffledPokedexData = shuffleArray(pokedexData.pokemon_entries);
+      const chosenPokemon = [];
 
-    // Async fetch details for all chosenPokemon
-    const promises = chosenPokemon.map(async (entry) => {
-      try {
-        const response = await fetch(
-          `https://pokeapi.co/api/v2/pokemon/${entry.pokemon_species.name}`,
-          {
-            mode: "cors",
-          }
-        );
-        if (response.status >= 400) {
-          throw new Error("pokemon fetch error");
-        }
-        return response.json();
-      } catch (error) {
-        setError(error);
-        return null;
+      // For inStock amount of times pop a pokemon and add to chosen array
+      for (let i = 0; i < stockNum; i += 1) {
+        chosenPokemon.push(shuffledPokedexData.pop());
       }
-    });
 
-    const pokemonDetailsData = await Promise.all(promises);
+      // Async fetch details for all chosenPokemon
+      const promises = chosenPokemon.map(async (entry) => {
+        try {
+          const response = await fetch(
+            `https://pokeapi.co/api/v2/pokemon/${entry.pokemon_species.name}`,
+            {
+              mode: "cors",
+            }
+          );
+          if (response.status >= 400) {
+            throw new Error("pokemon fetch error");
+          }
+          return response.json();
+        } catch (error) {
+          setError(error);
+          return null;
+        }
+      });
 
-    const filteredPokemonData = pokemonDetailsData.filter(
-      (data) => data !== null
-    );
+      const pokemonDetailsData = await Promise.all(promises);
 
-    setPokemon(filteredPokemonData);
-    setLoading(false);
-  };
+      const filteredPokemonData = pokemonDetailsData.filter(
+        (data) => data !== null
+      );
+
+      return filteredPokemonData;
+    };
+
+    getPokemonDetails(pokedexData)
+      .then((result) => setPokemonData(result))
+      .then(setLoading(false))
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, [pokedexData]);
+
+  return { pokemonData, loading, error };
 };
+
+export default usePreparePokemonData;
