d1d8d168f7d802c30c72b425ea32b291976141ea
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-14 21:56:07 -0400

    Add test for valid level

--- src/__tests__/generatePokemon.test.js
+++ src/__tests__/generatePokemon.test.js
@@ -3,8 +3,15 @@ import { describe, it, expect } from "vitest";
 import generatePokemon from "../utilities/generatePokemon";
 
 describe("generatePokemon utility", () => {
-  it("generates a pokemon with the correct name property", () => {
+  it("has the correct name property", () => {
     const testPokemon = generatePokemon("bidoof");
     expect(testPokemon).toStrictEqual({ name: "bidoof" });
+  });
+
+  it("has a level int between 1-100 inclusive", () => {
+    const testPokemon = generatePokemon("bidoof");
+    expect(Number.isInteger(testPokemon.level)).toBe(true);
+    expect(testPokemon.level).toBeGreaterThanOrEqual(1);
+    expect(testPokemon.level).toBeLessThanOrEqual(100);
   });
 });
