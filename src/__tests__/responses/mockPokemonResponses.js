ea80a4f3ebffa137067ad96c7136767196170ad5
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-15 15:41:53 -0400

    Export data

--- src/__tests__/responses/mockPokemonResponses.js
+++ src/__tests__/responses/mockPokemonResponses.js
@@ -1,11 +1,11 @@
-const bulbasaurBasicInfo = {
+export const bulbasaurBasicInfo = {
   sprites: {
     front_default:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
   },
 };
 
-const bulbasaurSpeciesInfo = {
+export const bulbasaurSpeciesInfo = {
   capture_rate: 45,
   evolves_from_species: null,
   gender_rate: 1,
