d75e0999eac909cc5858bb87d78b213d28a96986
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-14 21:03:23 -0400

    Add utility outline for generating a pokemon data object

---
+++ src/utilities/generatePokemon.js
@@ -0,0 +1,11 @@
+const generatePokemon = (name) => {
+  // Set pokemon's image property
+  // Set name
+  // Set random level 1-100
+  // Set gender based on gender_rate
+  // Set shiny based on chance
+  // Set price using various api data
+  // Set semi-random stats using base stats
+};
+
+export default generatePokemon;
