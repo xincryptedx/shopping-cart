6c3a2b6f661c94944509e3224e2aeb2d0cf13c86
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-15 15:35:34 -0400

    Add file containing mock responses for network requests

---
+++ src/__tests__/responses/mockPokemonResponses.js
@@ -0,0 +1,19 @@
+const bulbasaurBasicInfo = {
+  sprites: {
+    front_default:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+  },
+};
+
+const bulbasaurSpeciesInfo = {
+  capture_rate: 45,
+  evolves_from_species: null,
+  gender_rate: 1,
+  growth_rate: {
+    name: "medium-slow",
+  },
+  id: 1,
+  is_legendary: false,
+  is_mythical: false,
+  name: "bulbasaur",
+};
