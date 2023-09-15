313f1c388490f9ce58b3c1423388ae7de8ee26bf
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-15 15:51:36 -0400

    Add ivysaur responses

--- src/__tests__/responses/mockPokemonResponses.js
+++ src/__tests__/responses/mockPokemonResponses.js
@@ -17,3 +17,23 @@ export const bulbasaurSpeciesInfo = {
   is_mythical: false,
   name: "bulbasaur",
 };
+
+export const ivysaurBasicInfo = {
+  sprites: {
+    front_default:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png",
+  },
+};
+
+export const ivysaurSpeciesInfo = {
+  capture_rate: 45,
+  evolves_from_species: { name: "bulbasaur" },
+  gender_rate: 1,
+  growth_rate: {
+    name: "medium-slow",
+  },
+  id: 2,
+  is_legendary: false,
+  is_mythical: false,
+  name: "ivysaur",
+};
