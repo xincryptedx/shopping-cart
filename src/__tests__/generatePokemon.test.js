939992425739f1e572ff0b0a3533a66dc6e3191e
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-14 22:22:52 -0400

    Add dummy pokemon data and change name test to reflect array input

--- src/__tests__/generatePokemon.test.js
+++ src/__tests__/generatePokemon.test.js
@@ -3,9 +3,33 @@ import { describe, it, expect } from "vitest";
 import generatePokemon from "../utilities/generatePokemon";
 
 describe("generatePokemon utility", () => {
+  const dummyPokemonData = [
+    {
+      entry_number: 1,
+      pokemon_species: {
+        name: "bulbasaur",
+        url: "https://pokeapi.co/api/v2/pokemon-species/1/",
+      },
+    },
+    {
+      entry_number: 2,
+      pokemon_species: {
+        name: "ivysaur",
+        url: "https://pokeapi.co/api/v2/pokemon-species/2/",
+      },
+    },
+    {
+      entry_number: 3,
+      pokemon_species: {
+        name: "venusaur",
+        url: "https://pokeapi.co/api/v2/pokemon-species/3/",
+      },
+    },
+  ];
+
   it("has the correct name property", () => {
-    const testPokemon = generatePokemon("bidoof");
-    expect(testPokemon).toStrictEqual({ name: "bidoof" });
+    const testPokemon = generatePokemon(dummyPokemonData);
+    expect(testPokemon[0].name).toBe("bulbasaur");
   });
 
   it("has a level int between 1-100 inclusive", () => {
