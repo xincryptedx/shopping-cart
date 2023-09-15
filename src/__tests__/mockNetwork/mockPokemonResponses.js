8c9b2866e2ee93640130f7dbcc3fe3677e0182cf
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-15 15:57:38 -0400

    Add mew and mewtwo responses

--- src/__tests__/mockNetwork/mockPokemonResponses.js
+++ src/__tests__/mockNetwork/mockPokemonResponses.js
@@ -37,3 +37,43 @@ export const ivysaurSpeciesInfo = {
   is_mythical: false,
   name: "ivysaur",
 };
+
+export const mewtwoBasicInfo = {
+  sprites: {
+    front_default:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/150.png",
+  },
+};
+
+export const mewtwoSpeciesInfo = {
+  capture_rate: 3,
+  evolves_from_species: null,
+  gender_rate: -1,
+  growth_rate: {
+    name: "slow",
+  },
+  id: 150,
+  is_legendary: true,
+  is_mythical: false,
+  name: "mewtwo",
+};
+
+export const mewBasicInfo = {
+  sprites: {
+    front_default:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/151.png",
+  },
+};
+
+export const mewSpeciesInfo = {
+  capture_rate: 45,
+  evolves_from_species: null,
+  gender_rate: -1,
+  growth_rate: {
+    name: "medium-slow",
+  },
+  id: 151,
+  is_legendary: false,
+  is_mythical: true,
+  name: "mew",
+};
