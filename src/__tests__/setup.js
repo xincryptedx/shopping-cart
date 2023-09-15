35f6527b577f4bcb8ac2a5367bcdb042abda09dc
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-15 15:42:12 -0400

    Install and config msw for mocking network calls

--- src/__tests__/setup.js
+++ src/__tests__/setup.js
@@ -1,8 +1,37 @@
-import { afterEach } from "vitest";
+import { afterEach, afterAll, beforeAll } from "vitest";
 import { cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/vitest";
+import { setupServer } from "msw/node";
+import { rest } from "msw";
+
+import {
+  bulbasaurBasicInfo,
+  bulbasaurSpeciesInfo,
+} from "./responses/mockPokemonResponses";
+
+export const restHandlers = [
+  // Bulbasaur species info
+  rest.get(
+    "https://pokeapi.co/api/v2/pokemon-species/bulbasaur",
+    (req, res, ctx) => {
+      return res(ctx.status(200), ctx.json(bulbasaurSpeciesInfo));
+    }
+  ),
+
+  // Bulbasaur basic info
+  rest.get("https://pokeapi.co/api/v2/pokemon/bulbasaur", (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(bulbasaurBasicInfo));
+  }),
+];
+
+const server = setupServer(...restHandlers);
+
+beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
+
+afterAll(() => server.close());
 
 // Run a cleanup after each test case (clears jsdom)
 afterEach(() => {
+  server.resetHandlers();
   cleanup();
 });
