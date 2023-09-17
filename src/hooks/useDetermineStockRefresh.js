b167c36602fd47fb5d6f2774a5da8b193d7e3afd
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-17 16:42:56 -0400

    Add useEffect for setting refresh trigger

--- src/hooks/useDetermineStockRefresh.js
+++ src/hooks/useDetermineStockRefresh.js
@@ -1,13 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const useDetermineStockRefresh = () => {
   // Default refresh trigger state is false
   const [refreshStockTrigger, setRefreshStockTrigger] = useState(false);
 
-  // Check if there is a local storage entry
-  // If not, time for refresh
-  // If local storage entry, check its timestamp
-  // If more than an hour before current time, time for refresh
+  useEffect(() => {
+    // Check if there is a local storage entry
+    const storedPokemonData = localStorage.getItem("pokemonData");
+    const lastUpdateTime = localStorage.getItem("pokemonUpdateTime");
+
+    if (storedPokemonData) {
+      const currentTime = new Date();
+      const hourTimeout = 60 * 60 * 1000; // 60s 60m 1000ms
+
+      // If more than an hour before current time, refresh
+      if (currentTime - lastUpdateTime < hourTimeout) {
+        setRefreshStockTrigger(true);
+      } else {
+        setRefreshStockTrigger(false);
+      } // Data not older than an hour, no refresh
+    } else {
+      // No local data, refresh
+      setRefreshStockTrigger(true);
+    }
+  }, []);
 
   return [refreshStockTrigger, setRefreshStockTrigger];
 };
