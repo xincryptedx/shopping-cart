d8ae6ff1f5c8cec1d37ebdd1f4375be3446b2e37
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-17 17:33:40 -0400

    Also return next update time as state

--- src/hooks/useDetermineStockRefresh.js
+++ src/hooks/useDetermineStockRefresh.js
@@ -3,30 +3,37 @@ import { useEffect, useState } from "react";
 const useDetermineStockRefresh = () => {
   // Default refresh trigger state is false
   const [stockRefreshTrigger, setStockRefreshTrigger] = useState(false);
+  const [nextUpdate, setNextUpdate] = useState(null);
 
   useEffect(() => {
     // Check if there is a local storage entry
     const storedPokemonData = localStorage.getItem("pokemonData");
     const lastUpdateTime = localStorage.getItem("pokemonUpdateTime");
 
+    const hourTimeout = 60 * 60 * 1000; // 60s 60m 1000ms
+
     if (storedPokemonData && lastUpdateTime) {
       const lastTime = new Date(lastUpdateTime);
       const currentTime = new Date();
-      const hourTimeout = 60 * 60 * 1000; // 60s 60m 1000ms
+
+      // Calculate the time when data should be refreshed
+      const nextUpdateTime = new Date(lastTime.getTime() + hourTimeout);
 
       // If more than an hour before current time, refresh
-      if (currentTime - lastTime > hourTimeout) {
+      if (currentTime >= nextUpdateTime) {
         setStockRefreshTrigger(true);
+        setNextUpdate(new Date(currentTime.getTime() + hourTimeout));
       } else {
         setStockRefreshTrigger(false);
-      } // Data not older than an hour, no refresh
+        setNextUpdate(nextUpdateTime);
+      }
     } else {
-      // No local data, refresh
       setStockRefreshTrigger(true);
+      setNextUpdate(new Date().getTime() + hourTimeout);
     }
   }, []);
 
-  return stockRefreshTrigger;
+  return [stockRefreshTrigger, nextUpdate];
 };
 
 export default useDetermineStockRefresh;
