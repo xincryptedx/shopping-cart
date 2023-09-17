289fb2f69af95adfcc2651bc1c2ef2b6b50dff70
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-17 17:13:52 -0400

    Add placeholder timer

--- src/components/SortAndFilter/SortAndFilter.jsx
+++ src/components/SortAndFilter/SortAndFilter.jsx
@@ -1,8 +1,11 @@
 import styles from "./SortAndFilter.module.css";
 
 const SortAndFilter = () => {
+  const stockTimer = "48:36";
+
   return (
     <section className={styles.SortAndFilter}>
+      <p className={styles.stockTimer}>New Stock: {stockTimer}</p>
       <button>Sort</button>
       <button>Filter</button>
     </section>
