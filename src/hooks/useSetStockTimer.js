c54e8ad7810fe9737cb4ef05aa409756bdad2a05
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-18 19:53:55 -0400

    Determine the time left, format it, and return it

--- src/hooks/useSetStockTimer.js
+++ src/hooks/useSetStockTimer.js
@@ -4,12 +4,29 @@ const useSetStockTimer = (nextUpdate) => {
   const [stockTimer, setStockTimer] = useState(null);
 
   useEffect(() => {
+    const calculateTimeLeft = () => {
+      const difference = nextUpdate - new Date();
+
+      if (difference <= 0) {
+        return "00:00";
+      }
+
+      const minutes = Math.floor(difference / 60000);
+      const seconds = Math.floor((difference % 60000) / 1000);
+
+      const formattedMinutes = minutes.toString().padStart(2, "0");
+      const formattedSeconds = seconds.toString().padStart(2, "0");
+
+      return `${formattedMinutes}:${formattedSeconds}`;
+    };
+
     const timerInterval = setInterval(() => {
-      // Compare time and set state
+      const timeLeft = calculateTimeLeft();
+      setStockTimer(timeLeft);
     }, 1000);
 
     return () => clearInterval(timerInterval);
-  }, []);
+  }, [nextUpdate]);
 
   return stockTimer;
 };
