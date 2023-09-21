1ff0c6851e6a3e93f73c316f18fff56fa48c9e34
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-21 15:45:48 -0400

    Add pseudocode for handling button clicks

--- src/components/AddToCartButton/AddToCartButton.jsx
+++ src/components/AddToCartButton/AddToCartButton.jsx
@@ -3,20 +3,40 @@ import styles from "./AddToCartButton.module.css";
 import PropTypes from "prop-types";
 
 const AddToCartButton = ({ className, pokemonData, cart, setCart }) => {
-  const [numberInCart, setNumberInCart] = useState(0);
+  const [numberInCart, setNumberInCart] = useState(0); // For showing quantity on button
 
-  const updateCart = () => {
-    // Update the cart state to reflect new quantity using id
+  const addToCart = (id) => {
+    // Check if cart has an entry for the item yet
+    const foundEntry = cart.find((entry) => entry.id === id);
+    // If it does, increment the quantity prop, else create the entry with qauantity one
+    if (foundEntry) {
+      // Increment quantity property
+    } else {
+      // Create entry with quantity one
+    }
   };
 
+  const removeFromCart = (id) => {
+    // Check if cart has an entry for the item yet
+    const foundEntry = cart.find((entry) => entry.id === id);
+    // If the entry is found decrement its quantity
+    if (foundEntry) {
+      // Decrement quantity
+    }
+    // If quantity is >= 0 remove the whole cart entry
+    if (foundEntry && foundEntry.quantity >= 0) {
+      // Remove the entry from cart
+    }
+  };
+
+  // Custom hook that updates numberInCart
+
   const handleAddClick = () => {
-    setNumberInCart((previous) => previous + 1);
-    updateCart();
+    addToCart(pokemonData.id);
   };
 
   const handleSubtractClick = () => {
-    setNumberInCart((previous) => previous - 1);
-    updateCart();
+    removeFromCart(pokemonData.id);
   };
 
   return (
