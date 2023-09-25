b79dc1378e63030402d73e6939664ff9668810a0
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-25 15:34:15 -0400

    Render input based on state flag

--- src/components/AddToCartButton/AddToCartButton.jsx
+++ src/components/AddToCartButton/AddToCartButton.jsx
@@ -77,9 +77,14 @@ const AddToCartButton = ({ className, pokemonData, cart, setCart }) => {
           >
             -
           </button>
-          <p className={styles.quantity}>
-            x {numberInCart.toString().padStart(2, "0")}
-          </p>
+          {inputOpen ? (
+            <input />
+          ) : (
+            <p className={styles.quantity}>
+              x {numberInCart.toString().padStart(2, "0")}
+            </p>
+          )}
+
           <button onClick={handleAddClick} className={styles.addButton}>
             +
           </button>
