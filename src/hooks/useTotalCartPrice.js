78e6a434cdac42b42dd7f30dfc312485e69f76f2
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-26 17:57:51 -0400

    Remove console logs

--- src/hooks/useTotalCartPrice.js
+++ src/hooks/useTotalCartPrice.js
@@ -5,17 +5,13 @@ const useTotalCartPrice = (cart) => {
 
   useEffect(() => {
     if (Array.isArray(cart)) {
-      console.table(cart);
       const newTotal = cart.reduce((accumulator, currentValue) => {
         const lineTotal = currentValue.price * currentValue.quantity;
         return accumulator + lineTotal;
       }, 0);
-
-      console.log(newTotal);
       setTotalCartPrice(newTotal);
     }
   }, [cart]);
-  console.log(totalCartPrice);
   return totalCartPrice;
 };
 
