141fa70f7356de697fec818d5338dee781984634
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-26 17:54:49 -0400

    Hook wont run unless cart is an array

--- src/hooks/useTotalCartPrice.js
+++ src/hooks/useTotalCartPrice.js
@@ -4,14 +4,18 @@ const useTotalCartPrice = (cart) => {
   const [totalCartPrice, setTotalCartPrice] = useState(0);
 
   useEffect(() => {
-    const newTotal = cart.reduce((accumulator, currentValue) => {
-      const lineTotal = currentValue?.price * currentValue?.quantity || 0;
-      return accumulator + lineTotal;
-    }, 0);
+    if (Array.isArray(cart)) {
+      console.table(cart);
+      const newTotal = cart.reduce((accumulator, currentValue) => {
+        const lineTotal = currentValue.price * currentValue.quantity;
+        return accumulator + lineTotal;
+      }, 0);
 
-    setTotalCartPrice(newTotal);
+      console.log(newTotal);
+      setTotalCartPrice(newTotal);
+    }
   }, [cart]);
-
+  console.log(totalCartPrice);
   return totalCartPrice;
 };
 
