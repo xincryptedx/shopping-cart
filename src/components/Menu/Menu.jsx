606669fcdaf1401519524782b1952901f8d34e37
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-27 18:59:24 -0400

    Only set tabindex if menu open

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -3,16 +3,10 @@ import styles from "./Menu.module.css";
 import PropTypes from "prop-types";
 import cancelIcon from "../../assets/cancel.svg";
 import useSetStateOnKeydown from "../../hooks/useSetStateOnKeydown";
-import { useRef } from "react";
-import useSetFocusOnStateChange from "../../hooks/useSetFocusOnStateChange";
 
 const Menu = ({ className, navLinksOpen, setNavLinksOpen }) => {
   // Close NavLinks on escape key
   useSetStateOnKeydown("Escape", navLinksOpen, setNavLinksOpen, false);
-
-  // Set focus on first link when opened
-  const firstLink = useRef(null);
-  useSetFocusOnStateChange(firstLink.current, navLinksOpen, true);
 
   return (
     <section
@@ -33,9 +27,8 @@ const Menu = ({ className, navLinksOpen, setNavLinksOpen }) => {
           to="products"
           aria-label="prize counter"
           className={styles.productsBtn}
-          ref={firstLink}
-          onFocus={() => setNavLinksOpen(true)}
           onClick={() => setNavLinksOpen(false)}
+          tabIndex={navLinksOpen ? 0 : -1}
         >
           Prize Counter
         </Link>
