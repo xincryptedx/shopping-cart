b758bd55a591794a94ecb3d58ae140ff3a89c72f
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-28 19:18:13 -0400

    Pass shoppingCartOpen to NavBar

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -24,6 +24,7 @@ const MainLayout = () => {
       <NavBar
         menuOpen={menuOpen}
         setMenuOpen={setMenuOpen}
+        shoppingCartOpen={shoppingCartOpen}
         setShoppingCartOpen={setShoppingCartOpen}
         cart={cart}
       />
