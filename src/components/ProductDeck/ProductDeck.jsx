46b1dc3b285954586362f16b9fd378f748609090
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-28 19:09:22 -0400

    Pass state flags to ProductCard

--- src/components/ProductDeck/ProductDeck.jsx
+++ src/components/ProductDeck/ProductDeck.jsx
@@ -2,7 +2,13 @@ import ProductCard from "../ProductCard/ProductCard";
 import styles from "./ProductDeck.module.css";
 import PropTypes from "prop-types";
 
-const ProductDeck = ({ pokemonData, cart, setCart }) => {
+const ProductDeck = ({
+  pokemonData,
+  cart,
+  setCart,
+  shoppingCartOpen,
+  menuOpen,
+}) => {
   return (
     <section className={styles.ProductDeck}>
       {pokemonData.map((entry) => {
@@ -12,6 +18,8 @@ const ProductDeck = ({ pokemonData, cart, setCart }) => {
             key={entry.id}
             cart={cart}
             setCart={setCart}
+            shoppingCartOpen={shoppingCartOpen}
+            menuOpen={menuOpen}
           />
         );
       })}
@@ -23,6 +31,8 @@ ProductDeck.propTypes = {
   pokemonData: PropTypes.arrayOf(PropTypes.object),
   cart: PropTypes.arrayOf(PropTypes.object),
   setCart: PropTypes.func,
+  shoppingCartOpen: PropTypes.bool.isRequired,
+  menuOpen: PropTypes.bool.isRequired,
 };
 
 export default ProductDeck;
