48d8c8648c95feef876bb410dedc903f4c319dc4
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-29 18:47:09 -0400

    Rearrange elements

--- src/components/ContactInfo/ContactInfo.jsx
+++ src/components/ContactInfo/ContactInfo.jsx
@@ -3,15 +3,15 @@ import styles from "./ContactInfo.module.css";
 const ContactInfo = () => {
   return (
     <address className={styles.ContactInfo}>
+      <p>Phone: 555-555-5555</p>
+      <p>info@rocketgamecorner.com</p>
+      <p>Hours: All Day Every Day!</p>
       <div>
         <p>Celedon Game Corner</p>
         <p>123 Lucky Lane</p>
         <p>Celadon City, Kanto</p>
         <p>PokeZIP: 12345</p>
       </div>
-      <p>info@rocketgamecorner.com</p>
-      <p>Phone: 555-555-5555</p>
-      <p>Hours: All Day Every Day!</p>
     </address>
   );
 };
