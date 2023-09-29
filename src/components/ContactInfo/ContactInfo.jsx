fdd71b4453d0119ab7f974fa40352b3c94103fdc
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-29 18:50:56 -0400

    Add header

--- src/components/ContactInfo/ContactInfo.jsx
+++ src/components/ContactInfo/ContactInfo.jsx
@@ -3,11 +3,12 @@ import styles from "./ContactInfo.module.css";
 const ContactInfo = () => {
   return (
     <address className={styles.ContactInfo}>
+      <h1>Contact Us!</h1>
       <p>Phone: 555-555-5555</p>
       <p>info@rocketgamecorner.com</p>
       <p>Hours: All Day Every Day!</p>
       <div>
-        <p>Celedon Game Corner</p>
+        <p>Rocket Game Corner</p>
         <p>123 Lucky Lane</p>
         <p>Celadon City, Kanto</p>
         <p>PokeZIP: 12345</p>
