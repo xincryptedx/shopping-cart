6b7f4146df8d4c6c596c10d17b2f038cec4f5105
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-29 18:01:11 -0400

    Add ContactInfo component

--- src/routes/root/root.jsx
+++ src/routes/root/root.jsx
@@ -1,10 +1,12 @@
 import CallToAction from "../../components/CallToAction/CallToAction";
+import ContactInfo from "../../components/ContactInfo/ContactInfo";
 import styles from "./root.module.css";
 
 const Root = () => {
   return (
     <div className={styles.rootRoute}>
       <CallToAction />
+      <ContactInfo />
     </div>
   );
 };
