081c3e774c9850c8b6375c801c07801c0a64513a
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-29 18:11:26 -0400

    Add FAQ component and css module

---
+++ src/components/FrequentQuestions/FrequentQuestions.jsx
@@ -0,0 +1,33 @@
+import styles from "./FrequentQuestions.module.css";
+
+const FrequentQuestions = () => {
+  return (
+    <div className={styles.FAQ}>
+      <ul>
+        <li>
+          <p>Question: Answer</p>
+        </li>
+        <li>
+          <p>Question: Answer</p>
+        </li>
+        <li>
+          <p>Question: Answer</p>
+        </li>
+        <li>
+          <p>Question: Answer</p>
+        </li>
+        <li>
+          <p>Question: Answer</p>
+        </li>
+        <li>
+          <p>Question: Answer</p>
+        </li>
+        <li>
+          <p>Question: Answer</p>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default FrequentQuestions;
