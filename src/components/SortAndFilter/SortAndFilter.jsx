a2dbaaa99f3bbca329d60dc47cceecfda9f25264
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-29 16:21:03 -0400

    Translate region string into better form

--- src/components/SortAndFilter/SortAndFilter.jsx
+++ src/components/SortAndFilter/SortAndFilter.jsx
@@ -5,12 +5,27 @@ import PropTypes from "prop-types";
 const SortAndFilter = ({ nextUpdate, region }) => {
   const stockTimer = useSetStockTimer(nextUpdate);
 
+  const regionTranslations = {
+    "original-johto": "Johto",
+    "original-sinnoh": "Sinnoh",
+    "original-unova": "Unova",
+    "updated-alola": "Alola",
+  };
+
+  const prepareRegionString = (region) => {
+    const translatedRegion = regionTranslations[region];
+    if (translatedRegion) {
+      return translatedRegion;
+    }
+
+    // If no translation is found, capitalize the first letter.
+    return region[0].toUpperCase() + region.slice(1);
+  };
+
   return (
     <section className={styles.SortAndFilter}>
       <p className={styles.stockTimer}>New Stock: {stockTimer}</p>
-      <p className={styles.region}>
-        Region: {region[0].toUpperCase() + region.slice(1)}
-      </p>
+      <p className={styles.region}>Region: {prepareRegionString(region)}</p>
     </section>
   );
 };
