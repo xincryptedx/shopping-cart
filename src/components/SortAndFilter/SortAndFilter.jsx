ac20f32b96d468a3214105e5424c64cc850effb6
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-29 16:11:12 -0400

    Replace buttons with region label

--- src/components/SortAndFilter/SortAndFilter.jsx
+++ src/components/SortAndFilter/SortAndFilter.jsx
@@ -2,20 +2,22 @@ import useSetStockTimer from "../../hooks/useSetStockTimer";
 import styles from "./SortAndFilter.module.css";
 import PropTypes from "prop-types";
 
-const SortAndFilter = ({ nextUpdate }) => {
+const SortAndFilter = ({ nextUpdate, region }) => {
   const stockTimer = useSetStockTimer(nextUpdate);
 
   return (
     <section className={styles.SortAndFilter}>
       <p className={styles.stockTimer}>New Stock: {stockTimer}</p>
-      <button>Sort</button>
-      <button>Filter</button>
+      <p className={styles.region}>
+        Region: {region[0].toUpperCase() + region.slice(1)}
+      </p>
     </section>
   );
 };
 
 SortAndFilter.propTypes = {
   nextUpdate: PropTypes.instanceOf(Date),
+  region: PropTypes.string,
 };
 
 export default SortAndFilter;
