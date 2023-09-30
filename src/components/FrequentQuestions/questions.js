f6f0d2a5951a5a65551cbf8030395e0a5cb3b546
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-30 15:57:52 -0400

    Add placeholders

--- src/components/FrequentQuestions/questions.js
+++ src/components/FrequentQuestions/questions.js
@@ -5,6 +5,18 @@ const questions = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pharetra lorem eu dolor rhoncus, at scelerisque ligula gravida. Sed porta id mi sit amet convallis. Etiam iaculis massa sit amet lacus blandit sodales. Nulla ultrices velit a diam placerat congue. Pellentesque iaculis, ipsum quis eleifend dapibus, est dui eleifend ante, quis fermentum mi ligula quis nisl. Ut et ex dui. Integer id venenatis quam.",
     open: true,
   },
+  {
+    question: "How many programmers does it take to screw a lightbulb?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pharetra lorem eu dolor rhoncus, at scelerisque ligula gravida. Sed porta id mi sit amet convallis. Etiam iaculis massa sit amet lacus blandit sodales. Nulla ultrices velit a diam placerat congue. Pellentesque iaculis, ipsum quis eleifend dapibus, est dui eleifend ante, quis fermentum mi ligula quis nisl. Ut et ex dui. Integer id venenatis quam.",
+    open: false,
+  },
+  {
+    question: "How many programmers does it take to screw a lightbulb?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pharetra lorem eu dolor rhoncus, at scelerisque ligula gravida. Sed porta id mi sit amet convallis. Etiam iaculis massa sit amet lacus blandit sodales. Nulla ultrices velit a diam placerat congue. Pellentesque iaculis, ipsum quis eleifend dapibus, est dui eleifend ante, quis fermentum mi ligula quis nisl. Ut et ex dui. Integer id venenatis quam.",
+    open: false,
+  },
 ];
 
 export default questions;
