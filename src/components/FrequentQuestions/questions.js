b777cf7bf0157d185ef8c7c3d1f1cc9293fb461d
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-30 15:25:30 -0400

    Add file containing question data

---
+++ src/components/FrequentQuestions/questions.js
@@ -0,0 +1,10 @@
+const questions = [
+  {
+    question: "How many programmers does it take to screw a lightbulb?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pharetra lorem eu dolor rhoncus, at scelerisque ligula gravida. Sed porta id mi sit amet convallis. Etiam iaculis massa sit amet lacus blandit sodales. Nulla ultrices velit a diam placerat congue. Pellentesque iaculis, ipsum quis eleifend dapibus, est dui eleifend ante, quis fermentum mi ligula quis nisl. Ut et ex dui. Integer id venenatis quam.",
+    open: true,
+  },
+];
+
+export default questions;
