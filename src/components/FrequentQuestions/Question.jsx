8fce2794e7c9573ff632e3dc9c4d281a9559b967
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-30 15:42:42 -0400

    Destructure questions and setQuestions from props

--- src/components/FrequentQuestions/Question.jsx
+++ src/components/FrequentQuestions/Question.jsx
@@ -1,23 +1,46 @@
 import PropTypes from "prop-types";
 import styles from "./FrequentQuestions.module.css";
 
-const Question = (question, answer) => {
+const Question = ({
+  questionText,
+  answerText,
+  open,
+  questions,
+  setQuestions,
+}) => {
   // State for if the q is open
   // Only one can be open
   // If not this one selected then render as "closed"
   // If this one is selected then render as "open"
 
+  const toggleQuestion = (index) => {
+    setQuestions(
+      questions.map((faq, i) => {
+        if (i === index) {
+          faq.open = !faq.open;
+        } else {
+          faq.open = false;
+        }
+
+        return faq;
+      })
+    );
+  };
+
   return (
     <div className={styles.Question}>
-      <p className={styles.questionText}>{question}</p>
-      <p className={styles.answerText}>{answer}</p>
+      <p className={styles.questionText}>{questionText}</p>
+      <p className={styles.answerText}>{answerText}</p>
     </div>
   );
 };
 
-Question.PropTypes = {
-  question: PropTypes.string.isRequired,
-  answer: PropTypes.string.isRequired,
+Question.propTypes = {
+  questionText: PropTypes.string.isRequired,
+  answerText: PropTypes.string.isRequired,
+  open: PropTypes.bool.isRequired,
+  questions: PropTypes.arrayOf(PropTypes.object).isRequired,
+  setQuestions: PropTypes.func.isRequired,
 };
 
 export default Question;
