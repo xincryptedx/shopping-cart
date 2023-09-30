ef03801af5acf5418c25e121a3f3a06e5494af2d
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-09-30 17:36:01 -0400

    Add faq text

--- src/components/FrequentQuestions/questions.js
+++ src/components/FrequentQuestions/questions.js
@@ -1,20 +1,27 @@
 const questions = [
   {
-    question: "How many programmers does it take to screw a lightbulb?",
+    question: "Is this a front for Team Rocket?",
     answer:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pharetra lorem eu dolor rhoncus, at scelerisque ligula gravida. Sed porta id mi sit amet convallis. Etiam iaculis massa sit amet lacus blandit sodales. Nulla ultrices velit a diam placerat congue. Pellentesque iaculis, ipsum quis eleifend dapibus, est dui eleifend ante, quis fermentum mi ligula quis nisl. Ut et ex dui. Integer id venenatis quam.",
-    open: true,
-  },
-  {
-    question: "How many programmers does it take to screw a lightbulb?",
-    answer:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pharetra lorem eu dolor rhoncus, at scelerisque ligula gravida. Sed porta id mi sit amet convallis. Etiam iaculis massa sit amet lacus blandit sodales. Nulla ultrices velit a diam placerat congue. Pellentesque iaculis, ipsum quis eleifend dapibus, est dui eleifend ante, quis fermentum mi ligula quis nisl. Ut et ex dui. Integer id venenatis quam.",
+      "While we cannot disclose details of our parent companies, we can assure you that the Rocket Game Corner is a lawful, legitimate business. Any similarities to other organizations with 'Rocket' in the name are purely coincidental.",
     open: false,
   },
   {
-    question: "How many programmers does it take to screw a lightbulb?",
+    question: "How do you procure the prize Pokémon at the Rocket Game Corner?",
     answer:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pharetra lorem eu dolor rhoncus, at scelerisque ligula gravida. Sed porta id mi sit amet convallis. Etiam iaculis massa sit amet lacus blandit sodales. Nulla ultrices velit a diam placerat congue. Pellentesque iaculis, ipsum quis eleifend dapibus, est dui eleifend ante, quis fermentum mi ligula quis nisl. Ut et ex dui. Integer id venenatis quam.",
+      "Our prize Pokémon are sourced from the most reputable and non-suspicious Pokémon suppliers as well as donors trying to rehome unwanted Pokemon. Rest assured, all acquisitions are made through completely above-board channels.",
+    open: false,
+  },
+  {
+    question: "Is it true there is a secret basement?",
+    answer:
+      " While access is restricted for our patron's safety, our basement is merely a well-ventilated space for storing our extensive collection of gaming paraphernalia. There's absolutely nothing sinister happening down there.",
+    open: false,
+  },
+  {
+    question:
+      "The Porygon I won seems glitchy and unstable. Is something wrong with it?",
+    answer:
+      "Please do not be concerned - some minor glitches are to be expected when receiving a brand new Porygon fresh off the assembly line! Please note, however, that unofficial upgrade modules are not supported and their use may void your Porygon's limited warranty.",
     open: false,
   },
 ];
